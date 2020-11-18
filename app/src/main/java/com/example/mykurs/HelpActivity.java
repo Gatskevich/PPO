59bd79e05a0dd3c8c6a6b82150709a28f4b8065b
Rodion Gatskevich <rodion.gatskevich@mail.ru>
2020-11-18 09:38:05 +0300

    first commit

---
+++ app/src/main/java/com/example/mykurs/HelpActivity.java
@@ -0,0 +1,14 @@
+package com.example.mykurs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.mykurs.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
